aac32b031276ce93836f39a705df1869f74098e0
Munk91 <Mikkelbendixmunk@gmail.com>
2013-10-27 02:02:08 +0200

    Program

---
+++ ZMS_Trading/Main/GetStockData.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Main
+{
+    public class GetStockData
+    {
+        #region Fields
+        List<string> csvFile;
+        
+
+        #endregion
+
+        #region Methods
+
+        public List<string> ReadFromCSV()
+        {
+
+            csvFile = new List<string>();
+
+            string csvData;
+
+            WebClient web = new WebClient()
+            {
+                csvData = web.DownloadString("http://finance.yahoo.com/d/quotes.csv?s=AAPL+GOOG+MSFT&f=snbaopl1");
+            }
+            
+            csvFile.Add(csvData);
+
+            return csvFile;
+        }
+
+        #endregion
+
+
+    }
+}
